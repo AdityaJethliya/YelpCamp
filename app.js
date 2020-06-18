8e2fc7b3e0e40bd3ee69accf6f02e1c71051195b
aditya <adityajet99@gmail.com>
2020-06-18 16:24:13 +0000

    connect to atlas mongodb

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var commentRoutes = require("./routes/comments"),
 	campgroundRoutes = require("./routes/campgrounds"),
 	indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp",{useNewUrlParser: true});
+mongoose.connect("mongodb+srv://aditya:rusty@cluster0-pco7t.mongodb.net/yelpcamp?retryWrites=true&w=majority",{useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname+"/public"));
